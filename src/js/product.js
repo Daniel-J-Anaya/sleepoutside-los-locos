0bd327d3ff9b5b978d76c93bfe62eba533cf7344
Daniel-J-Anaya <ana15001@byui.edu>
2025-01-18 11:13:43 -0700

    Updated cart.js and product.js lint and format

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,11 @@
-import { setLocalStorage } from './utils.mjs';
-import { getLocalStorage } from './utils.mjs';
-import { findProductById } from './productData.mjs';
+import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage } from "./utils.mjs";
+import { findProductById } from "./productData.mjs";
 
 function addProductToCart(product) {
   // Retrieve the existing cart from localStorage
-  let cart = getLocalStorage('so-cart');
-  
+  let cart = getLocalStorage("so-cart");
+
   // If cart is not an array (i.e., a single product), make it an array
   if (!Array.isArray(cart)) {
     cart = cart ? [cart] : [];
@@ -15,7 +15,7 @@ function addProductToCart(product) {
   cart.push(product);
 
   // Save the updated cart back to localStorage
-  setLocalStorage('so-cart', cart);
+  setLocalStorage("so-cart", cart);
 }
 
 // add to cart button event handler
@@ -26,5 +26,5 @@ async function addToCartHandler(e) {
 
 // add listener to Add to Cart button
 document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+  .getElementById("addToCart")
+  .addEventListener("click", addToCartHandler);
