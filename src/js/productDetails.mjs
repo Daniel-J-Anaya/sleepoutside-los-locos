8f9f3769c83004358716df30b94fd07206e58717
Daniel-J-Anaya <ana15001@byui.edu>
2025-01-24 11:32:55 -0700

    Updated the getParam function and rewrote the productDetails function

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -1,23 +1,21 @@
 import { getLocalStorage, setLocalStorage } from './utils.mjs';
 import { findProductById } from './productData.mjs';
 
-// let data;
+let product = {};
 
-export function productDetails(productId, selector) {
+export async function productDetails(productId, selector) {
     // use findProductById to get the details for the current product. findProductById will return a promise! use await or .then() to process it
-    let product =  findProductById(productId).then(() =>{
-        // once we have the product details we can render out the HTML
-        console.log('Bello');
-        let productHTML = productDetailsTemplate(product)
-        let container = document.querySelector(selector)
-    
-        container.insertAdjacentHTML('afterbegin', productHTML)
-    
-        // add a listener to Add to Cart button
-        let button = document.querySelector('#addToCart')
-        button.addEventListener('click', addProductToCart(product))
-        }
-    )
+    let product =  await findProductById(productId);
+    // once we have the product details we can render out the HTML
+    console.log(product);
+    let productHTML = productDetailsTemplate(product);
+    let container = document.querySelector(selector);
+
+    container.insertAdjacentHTML('afterbegin', productHTML);
+
+    // add a listener to Add to Cart button
+    let button = document.querySelector('#addToCart')
+    button.addEventListener('click', addProductToCart(product))
     
 }
    
@@ -40,7 +38,7 @@ function productDetailsTemplate(product){
 
     <p class="product-card__price" id="productFinalPrice">${product.FinalPrice}</p>
 
-    <p class="product__color" id="productColorName">${product.Colors[1].ColorName}</p>
+    <p class="product__color" id="productColorName">${product.ColorsName}</p>
 
     <p class="product__description" id="productDescriptionHtmlSimple">${product.DescriptionHtmlSimple}</p>
 
