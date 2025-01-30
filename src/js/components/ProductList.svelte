12d271d6b455f432ee9f416f69918ab58e3a45cc
Isai Mar <mar18026@byui.edu>
2025-01-30 13:51:17 -0700

    Svelte list component
    Co-authored-by: Daniel Anaya <Daniel-J-Anaya@users.noreply.github.com>
    Co-authored-by: Ruby <Larson04@users.noreply.github.com>
    Co-authored-by: Andre Regino  <reg21003@byui.edu>
    Co-authored-by: Henry Dean Bondah <HenryBondah@users.noreply.github.com>

---
+++ src/js/components/ProductList.svelte
@@ -0,0 +1,22 @@
+<script>
+  import ProductSummary from "./ProductSummary.svelte";
+  import { getData } from "../productData.mjs";
+
+  let { category } = $props();
+
+  let promise = getData(category);
+</script>
+
+<h2>Top Products</h2>
+{#await promise}
+  <p>Loading</p>
+{:then data}
+  <ul class="product-list">
+    {#each data as product}
+      {#if product.Id != "880RT" && product.Id != "989CG"}
+        <li class="product-card"><ProductSummary {product} /></li>
+      {/if}
+    {/each}
+  </ul>
+{/await}
+
