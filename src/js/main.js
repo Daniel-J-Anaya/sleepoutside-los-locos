56b638faa6c79584f17272ce3c5d553f951fd895
DeipotentDanny <79887944+DeipotentDanny@users.noreply.github.com>
2025-02-01 18:52:17 -0700

    created alerts.json for sotring alert messages
    created alert.js to fetch alerts.json, parse, then add the alerts to all pages.
    
    added classes to style.css for front-end

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import ProductList from './components/ProductList.svelte';
 import { mount } from 'svelte';
+import { loadAlerts } from './alert.js';
 
 const productList = mount(ProductList, {
   target: document.querySelector('.products'),
