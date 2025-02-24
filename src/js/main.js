b912b63ce613b2a32814390adf46d45fe2051a1a
Andre Regino <reginopanai@gmail.com>
2025-02-23 23:54:41 -0700

    added functionallity to get set a local storage value if the user has visited the page before and display a banner

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,33 @@
-import { renderHeaderFooter } from './utils.mjs';
+import { renderHeaderFooter, getLocalStorage, setLocalStorage } from './utils.mjs';
 
 renderHeaderFooter();
+
+let visited = (getLocalStorage('visited').length != 0) ? true : false;
+
+if (visited){
+    console.log(visited);
+} else{
+    setLocalStorage('visited',true);
+    createBanner();
+
+}
+
+function createBanner(){
+    let bannerHTML = `
+    <div class="modal" id="modal">
+    <div class="modal-content">
+      <span class="close" id="close">&times;</span>
+      <h2>Win Big! 🎉</h2>
+      <p>Register today to enter our exclusive giveaway and stand a chance to win amazing prizes!</p>
+      <ul>
+        <li>💎 Free Giveaway for New Members</li>
+        <li>🎁 Win exclusive prizes</li>
+        <li>🚀 Easy registration - Get started now!</li>
+      </ul>
+      <button class="register-btn" id="register-btn">Register Now</button>
+    </div>
+  </div>
+    `
+
+    document.querySelector('main').insertAdjacentHTML('afterbegin',bannerHTML)
+}
