0c9b6961f977cec266c56ae8d18624b7e1ab92d6
Andre Regino <reginopanai@gmail.com>
2025-02-25 13:15:13 -0700

    finished the banner styles

--- src/js/main.js
+++ src/js/main.js
@@ -20,14 +20,21 @@ function createBanner(){
       <h2>Win Big! 🎉</h2>
       <p>Register today to enter our exclusive giveaway and stand a chance to win amazing prizes!</p>
       <ul>
-        <li>💎 Free Giveaway for New Members</li>
-        <li>🎁 Win exclusive prizes</li>
-        <li>🚀 Easy registration - Get started now!</li>
+        <li>Free Giveaway for New Members</li>
+        <li>Win exclusive prizes</li>
+        <li>Easy registration - Get started now!</li>
       </ul>
       <button class="register-btn" id="register-btn">Register Now</button>
     </div>
   </div>
     `
 
-    document.querySelector('main').insertAdjacentHTML('afterbegin',bannerHTML)
+  document.querySelector('main').insertAdjacentHTML('afterbegin',bannerHTML)
+
+  document.querySelector('.close').addEventListener('click', closeBanner)
 }
+
+function closeBanner(){
+  document.querySelector('.banner').style.display = 'none'
+}
+
