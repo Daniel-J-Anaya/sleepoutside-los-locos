9347cd4965d891aba0f13e93f475e76234b1e2ca
Ruby <pineapples1104@gmail.com>
2025-02-25 14:03:52 -0700

    Team 6 hopefully fixed

---
+++ src/product-list/product-list.js
@@ -0,0 +1,13 @@
+import ProductList from '../js/components/ProductList.svelte';
+import { mount } from 'svelte';
+import { getParam, renderHeaderFooter } from '../js/utils.mjs';
+ 
+console.log('hello')
+ 
+renderHeaderFooter();
+ 
+const category = getParam('category');
+const productList = mount(ProductList, {
+    target: document.querySelector('.products'),
+    props: { category: category},
+  });
